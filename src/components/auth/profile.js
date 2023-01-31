26142b8488053e9669b6803ac2ebe5e33f9031ab
Daniel Vacek <Raimen77@seznam.cz>
2023-01-31 01:23:38 +0100

    f

--- src/components/auth/profile.js
+++ src/components/auth/profile.js
@@ -1,27 +1,28 @@
-import { useState, useRef, useContext, useEffect } from "react";
-import { ClipLoader } from "react-spinners";
-import AuthContext from "../../store/auth-context";
-import classes from "./profile.module.css";
-import { storage } from "../../firebase";
+import { useState, useRef, useContext, useEffect } from 'react';
+import { ClipLoader } from 'react-spinners';
+import AuthContext from '../../store/auth-context';
+import classes from './profile.module.css';
+import { storage } from '../../firebase';
 
-import { getDownloadURL, ref, uploadString } from "@firebase/storage";
-const passwordChars = (value) => {
+import { getDownloadURL, ref, uploadString } from '@firebase/storage';
+const passwordChars = value => {
   const regexChars = /[$&+,:;=?@#|'<>.^*()%!-]/;
   return value.match(regexChars) && value.length >= 7;
 };
 
 const Profile = () => {
   const [selectedFile, setSelectedFile] = useState([]);
-  const [fileURL, setFileUrl] = useState("");
+  const [fileURL, setFileUrl] = useState('');
   const [formInputsValidity, setFormInputsValidity] = useState(true);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(false);
   const newPasswordInputRef = useRef();
+
   const authCtx = useContext(AuthContext);
   const { token } = authCtx;
 
   useEffect(() => {
-    getDownloadURL(ref(storage, "images")).then((url) => {
+    getDownloadURL(ref(storage, 'images')).then(url => {
       setFileUrl(url);
     });
   }, []);
@@ -30,10 +31,10 @@ const Profile = () => {
     if (isLoading) return;
     setIsLoading(true);
 
-    const imageRef = ref(storage, "images");
+    const imageRef = ref(storage, 'images');
 
     if (selectedFile) {
-      await uploadString(imageRef, selectedFile, "data_url").then(async () => {
+      await uploadString(imageRef, selectedFile, 'data_url').then(async () => {
         const downloadURL = await getDownloadURL(imageRef);
         setFileUrl(downloadURL);
       });
@@ -42,17 +43,17 @@ const Profile = () => {
     setIsLoading(false);
   };
 
-  const addImage = (e) => {
+  const addImage = e => {
     const reader = new FileReader();
     if (e.target.files[0]) {
       reader.readAsDataURL(e.target.files[0]);
     }
-    reader.onload = (readerEvent) => {
+    reader.onload = readerEvent => {
       setSelectedFile(readerEvent.target.result);
     };
   };
 
-  const formSubmit = (e) => {
+  const formSubmit = e => {
     e.preventDefault();
 
     const enteredNewPassword = newPasswordInputRef.current.value;
@@ -67,26 +68,26 @@ const Profile = () => {
     // add validation
     setIsLoading(true);
     fetch(
-      "https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyDiTdcWv-PzF8u_ILkBy4Re6RkMH5I11-A",
+      'https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyDiTdcWv-PzF8u_ILkBy4Re6RkMH5I11-A',
       {
-        method: "POST",
+        method: 'POST',
         body: JSON.stringify({
           idToken: token,
           password: enteredNewPassword,
           returnSecureToken: false,
         }),
-        headers: { "Content-Type": "application/json" },
+        headers: { 'Content-Type': 'application/json' },
       }
     )
-      .then((res) => {
+      .then(res => {
         setIsLoading(false);
         if (res.ok) {
           return res.json();
         } else {
-          res.json().then((data) => {
-            let errorMessage = "Authentication failed!";
+          res.json().then(data => {
+            let errorMessage = 'Authentication failed!';
             if (data) {
-              errorMessage = "You are already changed the password!";
+              errorMessage = 'You are already changed the password!';
             }
 
             setError(errorMessage);
@@ -94,7 +95,7 @@ const Profile = () => {
           });
         }
       })
-      .catch((err) => {
+      .catch(err => {
         console.log(err);
         setError(err.message);
       });
@@ -121,7 +122,7 @@ const Profile = () => {
       </div>
       <form className={classes.form} onSubmit={formSubmit}>
         <h2 className={classes.heading}>Change your password</h2>
-        <div className={classes["form-control"]}>
+        <div className={classes['form-control']}>
           <label htmlFor="new-password">new password</label>
           <input
             type="password"
@@ -130,9 +131,9 @@ const Profile = () => {
             minLength="7"
             ref={newPasswordInputRef}
           />
-          {error && <p className={classes["error-text"]}>{error}</p>}
+          {error && <p className={classes['error-text']}>{error}</p>}
           {!formInputsValidity && (
-            <p className={classes["error-text"]}>
+            <p className={classes['error-text']}>
               Your password must have one special char!
             </p>
           )}
